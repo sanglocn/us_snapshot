e9c39ce27cef087a9759f9cbbec972fdf3b96fbf
sanglocn <sanglocn@gmail.com>
2025-09-27 16:29:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -555,14 +555,14 @@ def make_ticker_figure(df_chart: pd.DataFrame, ticker: str, max_bars: int = 180)
     # Layout
     fig.update_layout(
         autosize=True,
-        height=650,
+        height=600,
         margin=dict(l=20, r=20, t=50, b=90),
         title=dict(text=f"{ticker} — Candlestick with SMA & Volume", x=0, xanchor="left"),
         legend=dict(orientation="h", yanchor="top", y=-0.25, xanchor="center", x=0.5),
         xaxis_rangeslider_visible=False,
         hovermode="x unified",
         template="plotly_white",
-        bargap=0.4   # 👈 controls space between candles (0 = no gap, 0.2 = nice spacing)
+        bargap=0.5   # 👈 controls space between candles (0 = no gap, 0.2 = nice spacing)
     )
 
     fig.update_yaxes(title_text="Price", row=1, col=1)
