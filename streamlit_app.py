53b7d601d1665bd5dde58c1e23acf530b324ebc4
sanglocn <sanglocn@gmail.com>
2025-09-27 16:35:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -555,7 +555,7 @@ def make_ticker_figure(df_chart: pd.DataFrame, ticker: str, max_bars: int = 180)
     # Layout
     fig.update_layout(
         autosize=True,
-        height=550,
+        height=500,
         margin=dict(l=20, r=20, t=50, b=90),     
         legend=dict(orientation="h", yanchor="top", y=-0.15, xanchor="center", x=0.5),
         xaxis_rangeslider_visible=False,
